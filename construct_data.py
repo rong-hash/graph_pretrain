c53237a8ea7cd751e3190194774b0e57f1eb674a
anonymousw2pgnn <124301228+anonymousw2pgnn@users.noreply.github.com>
2023-02-12 14:42:21 +0800

    Update construct_data.py

--- construct_data.py
+++ construct_data.py
@@ -1,22 +1,12 @@
 import argparse
-
 from loader import MoleculeDataset
 import itertools
-import torch.nn.functional as F
-import torch.optim as optim
-
-from tqdm import tqdm
-
 import pandas as pd
-
-import shutil
 import torch
 import numpy as np
 from itertools import compress
 from rdkit.Chem.Scaffolds import MurckoScaffold
 from collections import defaultdict
-from sklearn.model_selection import StratifiedKFold
-
 def generate_scaffold(smiles, include_chirality=False):
     """
     Obtain Bemis-Murcko scaffold from smiles
@@ -27,98 +17,6 @@ def generate_scaffold(smiles, include_chirality=False):
     scaffold = MurckoScaffold.MurckoScaffoldSmiles(
         smiles=smiles, includeChirality=include_chirality)
     return scaffold
-
-# from tensorboardX import SummaryWriter
-def scaffold_split(dataset, smiles_list, task_idx=None, null_value=0,
-                   frac_train=0.8, frac_valid=0.1, frac_test=0.1,
-                   return_smiles=False):
-    """
-    Adapted from https://github.com/deepchem/deepchem/blob/master/deepchem/splits/splitters.py
-    Split dataset by Bemis-Murcko scaffolds
-    This function can also ignore examples containing null values for a
-    selected task when splitting. Deterministic split
-    :param dataset: pytorch geometric dataset obj
-    :param smiles_list: list of smiles corresponding to the dataset obj
-    :param task_idx: column idx of the data.y tensor. Will filter out
-    examples with null value in specified task column of the data.y tensor
-    prior to splitting. If None, then no filtering
-    :param null_value: float that specifies null value in data.y to filter if
-    task_idx is provided
-    :param frac_train:
-    :param frac_valid:
-    :param frac_test:
-    :param return_smiles:
-    :return: train, valid, test slices of the input dataset obj. If
-    return_smiles = True, also returns ([train_smiles_list],
-    [valid_smiles_list], [test_smiles_list])
-    """
-    np.testing.assert_almost_equal(frac_train + frac_valid + frac_test, 1.0)
-
-    if task_idx != None:
-        # filter based on null values in task_idx
-        # get task array
-        y_task = np.array([data.y[task_idx].item() for data in dataset])
-        # boolean array that correspond to non null values
-        non_null = y_task != null_value
-        smiles_list = list(compress(enumerate(smiles_list), non_null))
-    else:
-        non_null = np.ones(len(dataset)) == 1
-        smiles_list = list(compress(enumerate(smiles_list), non_null))
-
-    # create dict of the form {scaffold_i: [idx1, idx....]}
-    all_scaffolds = {}
-    for i, smiles in tqdm(smiles_list):
-        scaffold = generate_scaffold(smiles, include_chirality=True)
-        if scaffold not in all_scaffolds:
-            all_scaffolds[scaffold] = [i]
-        else:
-            all_scaffolds[scaffold].append(i)
-
-    # sort from largest to smallest sets
-    all_scaffolds = {key: sorted(value) for key, value in all_scaffolds.items()}
-    all_scaffold_sets = [
-        scaffold_set for (scaffold, scaffold_set) in sorted(
-            all_scaffolds.items(), key=lambda x: (len(x[1]), x[1][0]), reverse=True)
-    ]
-    len_per_env = len( all_scaffold_sets ) // 5
-    envs = []
-
-    for i in range(5):
-        if len_per_env * (i + 1) < len(all_scaffold_sets ):
-            envs.append(list(itertools.chain(*( all_scaffold_sets [len_per_env * i:int(len_per_env * (i + 0.5))]))))
-        else:
-            envs.append(list(itertools.chain(*( all_scaffold_sets [len_per_env * i:int(len_per_env * (i + 0.5))]))))
-    return envs
-    # get train, valid test indices
-    # train_cutoff = frac_train * len(smiles_list)
-    # valid_cutoff = (frac_train + frac_valid) * len(smiles_list)
-    # train_idx, valid_idx, test_idx = [], [], []
-    # for scaffold_set in all_scaffold_sets:
-    #     if len(train_idx) + len(scaffold_set) > train_cutoff:
-    #         if len(train_idx) + len(valid_idx) + len(scaffold_set) > valid_cutoff:
-    #             test_idx.extend(scaffold_set)
-    #         else:
-    #             valid_idx.extend(scaffold_set)
-    #     else:
-    #         train_idx.extend(scaffold_set)
-    #
-    # assert len(set(train_idx).intersection(set(valid_idx))) == 0
-    # assert len(set(test_idx).intersection(set(valid_idx))) == 0
-    #
-    # train_dataset = dataset[torch.tensor(train_idx)]
-    # valid_dataset = dataset[torch.tensor(valid_idx)]
-    # test_dataset = dataset[torch.tensor(test_idx)]
-    #
-    # if not return_smiles:
-    #     return train_dataset, valid_dataset, test_dataset
-    # else:
-    #     train_smiles = [smiles_list[i][1] for i in train_idx]
-    #     valid_smiles = [smiles_list[i][1] for i in valid_idx]
-    #     test_smiles = [smiles_list[i][1] for i in test_idx]
-    #     return train_dataset, valid_dataset, test_dataset, (train_smiles,
-    #                                                         valid_smiles,
-    #                                                         test_smiles)
-
 
 def random_scaffold_split(dataset, smiles_list, task_idx=None, null_value=0,
                    frac_train=0.8, frac_valid=0.1, frac_test=0.1, seed=0):
@@ -210,7 +108,6 @@ def main():
 
     torch.manual_seed(args.runseed)
     np.random.seed(args.runseed)
-    device = torch.device("cuda:" + str(args.device)) if torch.cuda.is_available() else torch.device("cpu")
     if torch.cuda.is_available():
         torch.cuda.manual_seed_all(args.runseed)
     data_pre = "data/"
@@ -222,11 +119,6 @@ def main():
     for i in range(len(envs)):
         np.save(data_pre + "dataset/"+args.dataset+"/new_split"+str(i)+".npy",np.array(envs[i]))
 
-    # train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers)
-    # val_loader = DataLoader(valid_dataset, batch_size=args.batch_size, shuffle=False, num_workers=args.num_workers)
-    # test_loader = DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False, num_workers=args.num_workers)
-    #
-    #
 
 if __name__ == "__main__":
     main()
