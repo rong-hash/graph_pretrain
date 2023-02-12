bf2006df3f70e67b9e4bb1dca776aad5eb94b3b9
anonymousw2pgnn <124301228+anonymousw2pgnn@users.noreply.github.com>
2023-02-12 20:41:21 +0800

    Update generator_down.py

--- graph_classification/generator_down.py
+++ graph_classification/generator_down.py
@@ -1,46 +1,13 @@
-import argparse
 from utils.load_utils import MoleculeDataset
 import torch_geometric
 from utils.learner import estimate_graphon
 from tqdm import tqdm
 import numpy as np
-import os
-parser = argparse.ArgumentParser(description='Comparison for various methods on synthetic data')
-parser.add_argument('--r', type=int,
-                    default=1000,
-                    help='the resolution of graphon')
-parser.add_argument('--num-graphs', type=int,
-                    default=10,
-                    help='the number of synthetic graphs')
-parser.add_argument('--num-nodes', type=int, default=200,
-                    help='the number of nodes per graph')
-parser.add_argument('--graph-size', type=str, default='random',
-                    help='the size of each graph, random or fixed')
-parser.add_argument('--threshold-sba', type=float, default=0.1,
-                    help='the threshold of sba method')
-parser.add_argument('--threshold-usvt', type=float, default=0.1,
-                    help='the threshold of usvt method')
-parser.add_argument('--alpha', type=float, default=0.0003,
-                    help='the weight of smoothness regularizer')
-parser.add_argument('--beta', type=float, default=5e-3,
-                    help='the weight of proximal term')
-parser.add_argument('--gamma', type=float, default=0.1,
-                    help='the weight of gw term')
-parser.add_argument('--inner-iters', type=int, default=50,
-                    help='the number of inner iterations')
-parser.add_argument('--outer-iters', type=int, default=20,
-                    help='the number of outer iterations')
-parser.add_argument('--n-trials', type=int, default=2,
-                    help='the number of trials')
-parser.add_argument('--rw_hops', type=int, default=1,
-                    help='the number of trials')
-parser.add_argument('--down_data', type=str, default="bbbp",
-                    help='downstream data path')
-args = parser.parse_args()
-method = 'LG'
-file_path ="data/dataset/"
-save_path ="data//graphons/"
-def estimate_down_graphon(file_path,dataname):
+def estimate_generator_down(args):
+    file_path = args.file_path
+    dataname = args.down_data
+    save_path = args.save_path
+    method = args.method
     dataset = MoleculeDataset(file_path + dataname, dataset=dataname)
     datas=dataset
     predata_splits = ""
@@ -54,5 +21,4 @@ def estimate_down_graphon(file_path,dataname):
         stepfuncs.append(step_func)
     np.save(save_path + "down/" + dataname + "/graphons.npy", graphons)
     np.save(save_path + "down/" + dataname + "/func.npy", stepfuncs)
-estimate_down_graphon(file_path,args.down_data)
 
