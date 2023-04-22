0ddd2a8662ab40a288a4d8c8ded37183543becda
Zhirong Chen <zhirong.20@intl.zju.edu.cn>
2023-04-21 22:05:21 -0500

    update css style

--- app.py
+++ app.py
@@ -1,41 +1,37 @@
 from flask import Flask, render_template, request, jsonify
-from flask_uploads import UploadSet, configure_uploads, ALL
+from werkzeug.utils import secure_filename
 import os
 import threading
 
 app = Flask(__name__)
-app.config['UPLOADS_DEFAULT_DEST'] = 'uploads'
-uploaded_files = UploadSet('files', ALL)
-configure_uploads(app, uploaded_files)
+app.config['UPLOADS_DEFAULT_DEST'] = 'uploads/'
 
-def process_file(file_path, method):
-    # Add your algorithm implementations here
-    if method == "Node":
-        pass  # Replace with your algorithm for Method 1
-        return
-    elif method == "Graph":
-        pass  # Replace with your algorithm for Method 2
-        return 
-    else:
-        raise ValueError("Invalid method")
-
-    # Return the result or error message
-    return "Result or Error Message"
+def process_file(predata_path, downdata_path, method):
+    # Implement your algorithm here
+    result = "Processed files: {} and {} using method {}".format(predata_path, downdata_path, method)
+    return result
 
 @app.route('/', methods=['GET', 'POST'])
 def upload():
-    if request.method == 'POST' and 'file' in request.files:
+    if request.method == 'POST' and 'predata' in request.files and 'downdata' in request.files:
         method = request.form['method']
-        filename = uploaded_files.save(request.files['file'])
-        file_path = os.path.join(app.config['UPLOADS_DEFAULT_DEST'], 'files', filename)
-        
-        result = process_file(file_path, method)
-        
-        # Remove the file after processing
-        os.remove(file_path)
-        
+        predata = request.files['predata']
+        downdata = request.files['downdata']
+        predata_filename = secure_filename(predata.filename)
+        downdata_filename = secure_filename(downdata.filename)
+        predata_path = os.path.join(app.config['UPLOADS_DEFAULT_DEST'], predata_filename)
+        downdata_path = os.path.join(app.config['UPLOADS_DEFAULT_DEST'], downdata_filename)
+        predata.save(predata_path)
+        downdata.save(downdata_path)
+
+        result = process_file(predata_path, downdata_path, method)
+
+        # Remove the files after processing
+        os.remove(predata_path)
+        os.remove(downdata_path)
+
         return jsonify({'result': result})
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
